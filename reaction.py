467b08d57aa3a8687ff7e975d3cbdc60d21446a6
MJTKAI <127949176+MJTKAI@users.noreply.github.com>
2025-04-11 13:42:14 +0800

    Step 4: Detect button presses

--- reaction.py
+++ reaction.py
@@ -3,7 +3,13 @@ from time import sleep
 from random import uniform
 
 led = LED(4)
+left_button = Button(14)  # Button on GPIO14
+right_button = Button(15)  # Button on GPIO15
+def pressed(button):
+    print(f"Button {button.pin.number} pressed!")
 
 led.on()
 sleep(uniform(5, 10))  # Random delay between 5-10 seconds
 led.off()
+left_button.when_pressed = pressed
+right_button.when_pressed = pressed
